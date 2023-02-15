7f3ae2f7265616db4b6dc0edd749e3df90526685
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-02-15 11:44:12 -0500

    New folders

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({encoded:true}));
 
 app.listen(3000, function(){
     console.log("succes");
 })
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
